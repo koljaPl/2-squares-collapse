59eead3c13709e94fe4c2e76454dee20a601b3e3
nicklas <nikolya.plugin@gmail.com>
2026-04-09 23:05:12 +0200

    Added functions to calculate the time remaining until an object collides with a wall or another object, as well as a couple of helper functions.

--- physics/timeTo.go
+++ physics/timeTo.go
@@ -1,15 +1,59 @@
 package physics
 
 import (
+	"math"
+
 	"github.com/koljaPl/2-squares-collapse/physics/models"
 )
 
-func TimeToObjectCollision(object1, object2 models.Object) float64 {
-	// This function calculates the time until two objects collide and returns that time.
-	return 0
+func sideOfCube(mass, density float64) float64 {
+	side := math.Pow(mass/density, 1.0/3.0)
+	return side
 }
 
-func TimeToWall(object models.Object, simulation models.Simulation) (float64, string) {
+func relativeVelocity(object1, object2 models.Object) float64 {
+	relativeVx := object1.Vx - object2.Vx
+	relativeVy := object1.Vy - object2.Vy
+	return math.Sqrt(relativeVx*relativeVx + relativeVy*relativeVy)
+}
+
+func neededDistance(object1, object2 models.Object) float64 {
+	return (sideOfCube(object1.Mass, object1.Density) + sideOfCube(object2.Mass, object2.Density)) / 2
+}
+
+func currentDistance(object1, object2 models.Object) float64 {
+	return math.Sqrt(math.Pow(object1.X-object2.X, 2) + math.Pow(object1.Y-object2.Y, 2))
+}
+
+func TimeToObjectCollision(object1, object2 models.Object) float64 {
+	// This function calculates the time until two objects collide and returns that time.
+	neededDistance := neededDistance(object1, object2)
+
+	currentDistance := currentDistance(object1, object2)
+
+	relativeVelocity := relativeVelocity(object1, object2)
+
+	if relativeVelocity == 0 {
+		return math.Inf(1) // No collision if relative velocity is zero
+	}
+
+	time := (neededDistance - currentDistance) / relativeVelocity
+
+	return time
+}
+
+func TimeToWall(object models.Object, wallCoordinates float64) float64 {
+	// TODO: This code only works for 1D space; please fix it immediately
 	// This function calculates the time until the object collides with a wall and returns the time and the wall it will collide with.
-	return 0, ""
+	distanceToWall := math.Abs(wallCoordinates - object.X)
+
+	if object.Vx == 0 {
+		return math.Inf(1) // No collision if velocity is zero
+	}
+
+	side := sideOfCube(object.Mass, object.Density)
+
+	time := (distanceToWall - side/2 - object.X) / object.Vx
+
+	return time
 }
